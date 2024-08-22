6e87f99feeb0849fdab706b7489efce099efe966
Nina Viter <nina.viter@muehlemann-popp.ch>
2024-08-22 11:02:29 +0300

    add mock user list

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -3,7 +3,15 @@ import Link from "next/link";
 export default function Users() {
     return (
         <main className={'flex flex-col h-32 justify-center items-center gap-5'}>
-            <h1>Users</h1>
+            <h1>Users list:</h1>
+
+            <ul>
+                <li>User 1</li>
+                <li>User 2</li>
+            </ul>
+
+
+
 
             <Link className={'underline'} href={'/'}>Go to home</Link>
         </main>
