87ba0a7c3942c553f82adf34232839386662b3d2
Nina Viter <nina.viter@muehlemann-popp.ch>
2024-08-23 11:44:13 +0300

    fix types

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -5,8 +5,8 @@ import {revalidatePath} from "next/cache";
 
 export async function addUser(formData: FormData) {
 await prisma.user.create({data: {
-        name: formData.get('name'),
-        email: formData.get('email'),
+        name: formData.get('name') as string,
+        email: formData.get('email') as string,
     }})
     revalidatePath('/users')
 }
